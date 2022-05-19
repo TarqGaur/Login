49c8e2ce810c4ad4a380cdaf16c2a027283125bf
TarqGaur <89157771+TarqGaur@users.noreply.github.com>
2022-05-19 12:58:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function login(){
+        window.location="new.html";
+
+    localStorage.setItem("player_1",document.getElementById("input_1").value);
+    localStorage.setItem("player_2",document.getElementById("input_2").value);
+
+}
